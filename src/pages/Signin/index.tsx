ca7364b26a698f6af2e2230fe28cee9323902762
Everton <ton.oliveira@sempre.facig.edu.br>
2020-05-30 17:14:19 -0300

    Estruturado o Index do Login

---
+++ src/pages/Signin/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {FiLogIn} from 'react-icons/fi'
+
+import LogoImg from '../../assets/logo.svg'
+
+import {Container, Content, Background} from './styles'
+
+const SignIn: React.FC = () =>(
+  <Container>
+    <Content>
+      <img src={LogoImg} alt="GoBarber"/>
+
+      <form>
+        <h1>faça seu Logon</h1>
+
+        <input placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+
+        <button type="submit">Entrar</button>
+
+        <a href="forgot">Esqueci minha senha</a>
+      </form>
+
+      <a href="login">
+        <FiLogIn />
+        Ciar conta
+      </a>
+    </Content>
+
+    <Background />
+  </Container>
+)
+
+export default SignIn;
---
+++ src/pages/Signin/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+/*  */
+`
+export const Content = styled.div`
+/*  */
+`
+export const Background = styled.div`
+/*  */
+`
