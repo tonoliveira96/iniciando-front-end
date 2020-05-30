6f849a0b67c50534d6dbe6c27fc916a394649beb
Everton <ton.oliveira@sempre.facig.edu.br>
2020-05-30 18:04:36 -0300

    Finalizando estilização do Login

--- src/pages/Signin/styles.ts
+++ src/pages/Signin/styles.ts
@@ -1,48 +1,51 @@
 import styled from 'styled-components';
-import { shade} from 'polished'
+import { shade } from 'polished';
 import SignInbackgroungImg from '../../assets/sign-in-background.png';
-
 
 export const Container = styled.div`
   height: 100vh;
 
-  display:flex;
+  display: flex;
   align-items: stretch;
-
-`
+`;
 export const Content = styled.div`
-/*  */
-  display:flex;
+  /*  */
+  display: flex;
   flex-direction: column;
   align-items: center;
 
-  place-content:center;
+  place-content: center;
 
   width: 100%;
   max-width: 700px;
 
-  form{
+  form {
     margin: 80px 0;
     width: 340px;
     text-align: center;
 
-    h1{
+    h1 {
       margin-bottom: 24px;
     }
 
-    input{
+    input {
       background: #232129;
       border-radius: 10px;
       border: 2px solid #232129;
       padding: 16px;
       width: 100%;
+      color: #faede8;
 
-     & + input{
-      margin-top: 8px;
-     }
+      &:placeholder{
+        color: #666360;
+      }
+
+      & + input {
+        margin-top: 8px;
+      }
     }
 
-    button{
+    button {
       background: #ff9000;
       height: 56px;
       border-radius: 10px;
@@ -54,17 +57,45 @@ export const Content = styled.div`
       margin-top: 16px;
       transition: background-color 0.2s;
 
-      &:hover{
-        background: ${shade(0.3, '#ff9000')}
+      &:hover {
+        background: ${shade(0.3, '#ff9000')};
       }
-     }
+    }
+
+    a {
+      color: #faede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${(shade(0.3), '#Faede8')};
+      }
+    }
   }
-`
+
+  > a {
+    color: #ff9000;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
+
+    display: flex;
+    align-items: center;
+
+    svg{
+      margin-right: 16px;
+    }
+    &:hover {
+        color: ${shade(0.3, '#ff9000')};
+      }
+  }
+`;
 export const Background = styled.div`
-/*  */
+  /*  */
   flex: 1;
-  background:url(${SignInbackgroungImg}) no-repeat center;
+  background: url(${SignInbackgroungImg}) no-repeat center;
   background-size: cover;
-
-
-`
+`;
