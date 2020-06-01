c871cdd4de9d1c99da929d61b14b6bc4f1be32d7
Everton <ton.oliveira@sempre.facig.edu.br>
2020-06-01 20:02:37 -0300

    Estlos do Componente Button

--- src/pages/Signin/styles.ts
+++ src/pages/Signin/styles.ts
@@ -28,23 +28,6 @@ export const Content = styled.div`
       margin-bottom: 24px;
     }
 
-    button {
-      background: #ff9000;
-      height: 56px;
-      border-radius: 10px;
-      border: 0;
-      padding: 0 16px;
-      color: #312e38;
-      width: 100%;
-      font-weight: 500;
-      margin-top: 16px;
-      transition: background-color 0.2s;
-
-      &:hover {
-        background: ${shade(0.3, '#ff9000')};
-      }
-    }
-
     a {
       color: #faede8;
       display: block;
