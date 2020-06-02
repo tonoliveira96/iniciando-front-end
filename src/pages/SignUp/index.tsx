e4233cac00e2aeda5d379e29fb9c9d7318d67260
Everton <ton.oliveira@sempre.facig.edu.br>
2020-06-01 21:55:20 -0300

    Validando cadastro

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import {Form} from '@unform/web';
+import * as Yup from 'yup';
 
 import LogoImg from '../../assets/logo.svg';
 
@@ -10,9 +11,23 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: Object): void{
+  const handleSubmit= useCallback(async(data: Object)=>{
     console.log(data)
-  }
+    try{
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string().required('E-mail obrigatório').email(),
+        password: Yup.string().min(6, 'Mínimo 6 dígitos')
+      })
+
+      await schema.validate(data,{
+        abortEarly: false
+      })
+
+    }catch(err){
+      console.log(err)
+    }
+  }, [])
 
   return(
     <Container>
