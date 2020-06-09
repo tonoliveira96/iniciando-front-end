f954e4eb8d206c27c8f18b24515977944c1dcc6f
Everton <ton.oliveira@sempre.facig.edu.br>
2020-06-09 20:28:20 -0300

    Mostra o Toast automático

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,46 +1,33 @@
 import React from 'react';
 import {FiAlertCircle, FiXCircle} from 'react-icons/fi';
 
+import {ToastMessage} from '../../hooks/toast'
 import {Container, Toast} from './styles';
 
-const ToastContainer: React.FC = () =>{
+interface ToastContainerProps{
+  messages: ToastMessage[]
+}
+
+const ToastContainer: React.FC<ToastContainerProps> = ({messages}) =>{
   return(
     <Container>
-      <Toast hasDescription>
+      {messages.map(message=>(
+        <Toast
+        key={message.id}
+        type={message.type}
+        hasDescription={!!message.description}>
         <FiAlertCircle size={20}/>
 
         <div>
-          <strong>Aconteceu um erro</strong>
-          <p>Não foi possive realizar login</p>
+          <strong>{message.title}</strong>
+      {message.description && <p>{message.description}</p>}
         </div>
         <button type="button">
           <FiXCircle size={18}/>
         </button>
       </Toast>
+      ))}
 
-      <Toast type='success'hasDescription>
-        <FiAlertCircle size={20}/>
-
-        <div>
-          <strong>Aconteceu um erro</strong>
-          <p>Não foi possive realizar login</p>
-        </div>
-        <button type="button">
-          <FiXCircle size={18}/>
-        </button>
-      </Toast>
-
-      <Toast type='error' hasDescription={false}>
-        <FiAlertCircle size={20}/>
-
-        <div>
-          <strong>Aconteceu um erro</strong>
-          <p></p>
-        </div>
-        <button type="button">
-          <FiXCircle size={18}/>
-        </button>
-      </Toast>
     </Container>
   )
 }
--- src/hooks/toast.tsx
+++ src/hooks/toast.tsx
@@ -1,17 +1,35 @@
-import React, {createContext, useCallback, useContext } from 'react';
-
+import React, {createContext, useCallback, useContext, useState } from 'react';
+import {uuid} from 'uuidv4';
 import ToastContainer from '../components/ToastContainer'
 
+export interface ToastMessage{
+  id: string,
+  type: 'success' | 'error' | 'info',
+  title: string;
+  description?: string;
+
+}
 interface ToastContextData{
-  addToast(): void;
+  addToast(message: Omit<ToastMessage, 'id'> ): void;
   removeToast(): void;
 }
 
 const ToastContext = createContext<ToastContextData>({} as ToastContextData);
 
 const ToastProvider : React.FC = ({children})=>{
-  const addToast =useCallback(()=>{
-    console.log('addToast')
+  const [message, setMessage] = useState<ToastMessage[]>([])
+
+  const addToast =useCallback(({type, title, description}: Omit<ToastMessage, 'id'>)=>{
+    const id = uuid();
+
+    const toast ={
+      id,
+      type,
+      title,
+      description
+    }
+
+    setMessage((state)=>[...state, toast])
   },[])
 
   const removeToast= useCallback(()=>{
@@ -21,7 +39,7 @@ const ToastProvider : React.FC = ({children})=>{
   return(
     <ToastContext.Provider value={{addToast, removeToast}}>
       {children}
-      <ToastContainer />
+      <ToastContainer messages={message} />
     </ToastContext.Provider>
   )
 }
