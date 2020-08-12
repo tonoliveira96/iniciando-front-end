18c21c4ca0703c09f8a0c92c5707956e6db894bf
Everton <ton.oliveira@sempre.facig.edu.br>
2020-08-12 20:42:58 -0300

    Próximo agendamento

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,38 +1,70 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
-  const { signOut, user} = useAuth();
+  const { signOut, user } = useAuth();
 
-  return(
+  return (
     <Container>
-    <Header>
-      <HeaderContent>
-        <img src={logoImg} alt="GoBarber" />
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
 
-        <Profile>
-          <img
-            src={user.avatar_url}
-            alt={user.name}
-          />
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
 
-          <div>
-            <span>Bem-vindo, </span>
-            <strong>{user.name}</strong>
-          </div>
-        </Profile>
+            <div>
+              <span>Bem-vindo, </span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
 
-        <button type="button" onClick={signOut}>
-          <FiPower />
-        </button>
-      </HeaderContent>
-    </Header>
-  </Container>
-  )
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+
+            <div>
+              <img src="https://avatars1.githubusercontent.com/u/43159625?s=460&u=08567372edcab56e9f803be46ed3adb47a4843f5" alt=""/>
+
+              <strong>Fulano</strong>
+              <span>
+                <FiClock />
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+
+        <Calendar></Calendar>
+      </Content>
+    </Container>
+  );
 };
 
 export default Dashboard;
